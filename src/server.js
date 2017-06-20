e6ccfa1bd83c3f6507b4c54f3008d7846f28c4ea
michal <michal.labuda@edps.com.pl>
2017-06-20 20:52:36 +0200

    Inicjalny commit - Hello World

---
+++ src/server.js
@@ -0,0 +1,10 @@
+
+console.log('hello from Node.js server');
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(2020); 
