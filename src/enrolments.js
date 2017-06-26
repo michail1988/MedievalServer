0107600bd173568cb2d061a32a53c2575effb01e
michal <michal.labuda@edps.com.pl>
2017-06-26 22:40:07 +0200

    Operacje bazodanowe: Enrolments

---
+++ src/enrolments.js
@@ -0,0 +1,11 @@
+var db = require('./db');
+
+exports.getAll = function(done) {
+	db.get().query('SELECT NAME, SURNAME, DATE, EMAIL FROM MED_ENROLMENTS',
+			function(err, rows) {
+				if (err)
+					return done(err)
+				done(null, rows)
+				console.log('Erolments selected')
+			})
+}
