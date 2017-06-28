ebd28be7d645359d312e25cc9355829cb2d853a0
michal <michal.labuda@edps.com.pl>
2017-06-28 23:55:57 +0200

    body-parser, tworzenie zgloszenia (enrolment)

--- src/db/enrolments.js
+++ src/db/enrolments.js
@@ -9,3 +9,15 @@ exports.getAll = function(done) {
 				console.log('Erolments selected')
 			})
 }
+
+exports.createEnrolment = function(name, surname, date, email, done) {
+	  var values = [name, surname, new Date(), email]
+	  
+	  console.log('Create enrolments: ' + values)
+	  db.get().query('INSERT INTO MED_ENROLMENTS (NAME, SURNAME, DATE, EMAIL) VALUES(?, ?, ?, ?)', values, function(err, result) {
+		  if (err) return done(err)
+		    done(null, result)
+		    
+	   
+	  })
+	}
